70fef52a4970b50f2d90686d3bd5bea90098461f
Mikołaj Zuchowicz <57917714+Sh1zum@users.noreply.github.com>
2023-06-13 10:21:33 +0200

    connection

--- Connection.php
+++ Connection.php
@@ -10,9 +10,9 @@ class Connection
     public function connect(): PDO
     {
         $user = '37435088_currencies';
-        $pass = 'I1LQbttD';
+        $pass = 'xthQuKL_';
         try {
-            return new PDO('mysql:host=serwer2307765.home.pl;dbname=37435088_currencies', $user, $pass);
+            return new PDO('mysql:host=serwer2307765.home.pl:3380;dbname=37435088_currencies', $user, $pass);
 
         } catch (PDOException $e) {
             throw new ConnectionException('There is a problem with connection to database');
